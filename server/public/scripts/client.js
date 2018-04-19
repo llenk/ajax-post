302502c49a05096f381c1e92ab621991e4b46937
llenk <ellenkeal@gmail.com>
2018-04-19 13:04:35 -0500

    add records to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,4 +3,29 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('ayy');
+    $.ajax({
+        type: 'GET', //this can also be method
+        url: '/records'
+    })
+    .then(function(response) {
+        console.log(response);
+        response.forEach(function(record) {
+            $('#records').append(`<tr>
+                <td>${record.title}</td>
+                <td>${record.artist}</td>
+                <td>${record.year}</td>
+                <td>${record.cost}</td>
+                </tr>`);
+        });
+    });
 }
+
+function formatRecords(record) {
+    let returnString = '<tr>';
+    returnString += '<td>' + record.title + '</td>';
+    returnString += '<td>' + record.artist + '</td>';
+    returnString += '<td>' + record.year + '</td>';
+    returnString += '<td>' + record.cost + '</td></tr>';
+    console.log(returnString);
+    return returnString;
+};
--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
+//put consts and requires on top
 const express = require('express');
-
 const app = express();
 const PORT  = 5000;
 
+//this will live in the database eventually
+const records = require('./modules/record-collection');
+
+//then static files
 app.use(express.static('server/public'));
+
+//routes (get, post, put delete requests)
+//when we have a ton, we'll put them in modules
+app.get('/records', function(req, res) {
+    res.send(records);
+});
 
 app.listen(PORT, () => {
     console.log(`up and running on port ${PORT}`);
 });
+
